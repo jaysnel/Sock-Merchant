08e27104bc11f9251ea6f8c91e23631aac3cfda6
Jaylan Snelson <jaylan210@gmail.com>
2020-06-05 11:53:51 -0400

    Create template.js

---
+++ template.js
@@ -0,0 +1,32 @@
+// John works at a clothing store. He has a large pile of socks that he must pair by color for sale. Given an array of integers
+// representing the color of each sock, determine how many pairs of socks with matching colors there are.
+
+// FUNCTION DESCRIPTION
+
+// It must return an integer representing the number of matching pairs of socks that are available.
+
+// sockMerchant has the following parameter(s):
+
+//     n: the number of socks in the pile
+//     ar: the colors of each sock
+
+// Return the total number of matching pairs of socks that John can sell.
+
+// SAMPLE INPUT
+// 9
+// 10 20 20 10 10 30 50 10 20
+
+// SAMPLE OUTPUT
+// 3
+
+// TEST VARIABLES
+// let n = 10;
+// let arr = [1,1,3,1,2,1,3,3,3,3]
+//let n = 9;
+//let arr = [10,20,20,10,10,30,50,10,20];
+
+function sockMerchant(n, ar) {
+  
+}
+
+sockMerchant(n,arr)
